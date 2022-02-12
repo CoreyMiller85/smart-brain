0cec7828e9eae70b6a140a30c4811257f0ed07e1
Corey Miller <coreymiller454@gmail.com>
2022-02-12 02:23:50 -0500

    add dropdown on profile image

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,14 +1,42 @@
 import { useState } from 'react';
+import {
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Dropdown,
+} from 'reactstrap';
 
 const ProfileIcon = (props) => {
   const [dropDownOpen, setDropDownOpen] = useState(false);
+
+  const toggle = () => {
+    setDropDownOpen(!dropDownOpen);
+  };
   return (
     <div className='pa4 tc'>
-      <img
-        src='http://tachyons.io/img/logo.jpg'
-        alt='avatar'
-        className='br-100 ba h3 w3 dib'
-      />
+      <div className='d-flex justify-content-center p-5'>
+        <Dropdown isOpen={dropDownOpen} toggle={toggle}>
+          <DropdownToggle
+            tag='span'
+            onClick={toggle}
+            data-toggle='dropdown'
+            aria-expanded={dropDownOpen}
+          >
+            <div className='pa4 tc'>
+              <img
+                src='http://tachyons.io/img/logo.jpg'
+                alt='avatar'
+                className='br-100 ba h3 w3 dib'
+              />
+            </div>
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>View Profile</DropdownItem>
+
+            <DropdownItem>Sign Out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
     </div>
   );
 };
